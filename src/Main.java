19b12fca3ee68f4700a8a8a99de078464b332552
002FAQ744 <ashishkumarpatel855@gmail.com>
2022-04-24 13:42:20 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        //print the 7 tables
+        //print the 7 tables 12233
         int table =7;
         for(int i=1; i<=10; i++) {
             System.out.printf("%d*%d =%d",table,i,table*i).println();
