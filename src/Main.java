e1b0c8371910bbcdec57974e01580f30da29dfa6
002FAQ744 <ashishkumarpatel855@gmail.com>
2022-04-24 13:38:59 +0530

    git commit -am "table"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        //print the tables
+        //print the 7 tables
         int table =7;
         for(int i=1; i<=10; i++) {
             System.out.printf("%d*%d =%d",table,i,table*i).println();
