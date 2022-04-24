94d5da7af65b07dd3f4ac0b1639dcdce72f2f5cb
Ashish Patel1 <ashish.patel1@ibm.com>
2022-04-24 13:34:25 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        //print the tables
+        int table =7;
+        for(int i=1; i<=10; i++) {
+            System.out.printf("%d*%d =%d",table,i,table*i).println();
+        }
+    }
+}
